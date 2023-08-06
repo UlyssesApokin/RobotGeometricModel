ca6d547c86db11f02f4566645d3f341686053d89
Ulysses Apokin <aoipkn@yandex.ru>
2023-08-06 17:10:05 +0300

    new: writing interpritator

--- rgm2.c
+++ rgm2.c
@@ -14,9 +14,50 @@ struct Robot
 	double *rmtrx;
 };
 
-void init_rgm(FILE *filename, struct Robot *robot)
+void analyze_text(const char *str, struct Robot *robot);
+
+void init_rgm(const char *filename, struct Robot *robot)
 {
-	
+	char c;
+	char *str;
+	int size_str = 1;
+	FILE *robot_characteristics;
+	robot_characteristics = fopen(filename, "r");
+	if (!robot_characteristics)
+	{
+		perror(filename);
+		exit(1);
+	}
+	str = malloc(size_str * sizeof(char));
+	while ((c = fgetc(robot_characteristics)) != EOF)
+	{
+		const char *comment = "COMMENT{";
+		enum {start = 33, end = 126};
+		if (c >= start && c <= end)
+		{
+			str[size_str - 1] = c;
+			
+			if (strcmp(str, comment)
+			{
+				
+			}
+			
+			
+			str = realloc(str, (size_str) * sizeof(char));
+			if (!str)
+			{
+				perror(str);
+				exit(2);
+			}
+		}
+	}
+	fclose(robot_characteristics);
+	free(str);
+}
+
+void analyze_text(const char *str, struct Robot *robot)
+{
+
 }
 
 int main (int argc, char **argv)
