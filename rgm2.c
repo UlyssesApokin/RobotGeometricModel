541f55729f5a04b6db6fc13c3f9a0d5d8b711035
Ulysses Apokin <aoipkn@yandex.ru>
2023-08-09 18:51:17 +0300

    fix: error realloc(): invalid old size

--- rgm2.c
+++ rgm2.c
@@ -21,12 +21,19 @@ void init_rgm(const char *filename, struct Robot *robot)
 {
 	char c;
 	char *str;
+	char *type_pair, *lenght_pair, *coords, *rotation_matrix;
 	const char *TYPE_PAIR = "TYPE_PAIR";
-	const char *LENGNT_PAIR = "LENGHT_PAIR";
+	const char *LENGTH_PAIR = "LENGTH_PAIR";
 	const char *COORDS = "COORDS";
 	const char *ROTATION_MATRIX = "ROTATION_MATRIX";
 	int size_str = 1;
+	enum {tp = 9, lp = 11, co = 6, rm = 15, cm = 7};
+	enum {start = 33, end = 126};
 	FILE *robot_characteristics;
+	type_pair = malloc(sizeof(char));
+	lenght_pair = malloc(sizeof(char));
+	coords = malloc(sizeof(char));
+	rotation_matrix = malloc(sizeof(char));
 	robot_characteristics = fopen(filename, "r");
 	if (!robot_characteristics)
 	{
@@ -37,10 +44,6 @@ void init_rgm(const char *filename, struct Robot *robot)
 	str[size_str] = '\0';
 	while ((c = fgetc(robot_characteristics)) != EOF)
 	{
-		
-		enum {tp = 9, lp = 11, co = 6, rm = 15, cm = 7};
-		char *type_pair, *lenght_pair, *coords, *rotation_matrix;
-		enum {start = 33, end = 126};
 		if (c >= start && c <= end)
 		{
 			str[size_str - 1] = c;
@@ -51,11 +54,11 @@ void init_rgm(const char *filename, struct Robot *robot)
 					type_pair = malloc((size_str+1) * sizeof(char));
 					strcpy(type_pair, str);
 				}
-				if (!strncmp(str, LENGNT_PAIR, sizeof(char)*lp))
+				if (!strncmp(str, LENGTH_PAIR, sizeof(char)*lp))
 				{
 					lenght_pair = malloc((size_str+1) * sizeof(char));
 					strcpy(lenght_pair, str);
-					
+
 				}
 				if (!strncmp(str, COORDS, sizeof(char)*co))
 				{
@@ -69,20 +72,26 @@ void init_rgm(const char *filename, struct Robot *robot)
 				}
 				size_str = 0;
 			}
-			printf("%s\n", type_pair);
 			size_str++;
 			str = realloc(str, (size_str+1) * sizeof(char));
-			str[size_str] = '\0';
 			if (!str)
 			{
 				perror(str);
 				exit(2);
 			}
+			str[size_str] = '\0';
 		}
 	}
-	
+	printf("%s\n", type_pair);
+	printf("%s\n", lenght_pair);
+	printf("%s\n", coords);
+	printf("%s\n", rotation_matrix);
 	fclose(robot_characteristics);
 	free(str);
+	free(type_pair);
+	free(lenght_pair);
+	free(coords);
+	free(rotation_matrix);
 }
 
 void analyze_text(const char *str, struct Robot *robot)
@@ -93,6 +102,9 @@ void analyze_text(const char *str, struct Robot *robot)
 int main (int argc, char **argv)
 {
 	struct Robot robot;
-	init_rgm(argv[1], &robot);
+	if (argc > 1)
+		init_rgm(argv[1], &robot);
+	else
+		puts("To few arguments");
 	return 0;
 }
