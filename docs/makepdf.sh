85ff8da5c37c4f54a6aebddec28556f03eda1a77
UlyssesApokin <aoipkn@yandex.ru>
2023-09-30 10:34:18 +0300

    any number of pages in a document

--- docs/makepdf.sh
+++ docs/makepdf.sh
@@ -4,5 +4,5 @@ pdflatex kurs.tex
 qpdf kurs.pdf --underlay frame-titul-sheet-gost2104-68.pdf --to=1 -- final.pdf
 qpdf final.pdf --underlay frame-main-sheet-gost2104-68.pdf --to=2 -- final2.pdf
 qpdf final2.pdf --underlay frame-сontent-sheet-gost2104-68.pdf --to=3 -- final.pdf
-qpdf final.pdf --underlay frame-main-sheet-gost2104-68.pdf --from= --repeat=1 --to=4-14 -- final2.pdf
+qpdf final.pdf --underlay frame-main-sheet-gost2104-68.pdf --from= --repeat=1 --to=4-z -- final2.pdf
 mv final2.pdf final.pdf
