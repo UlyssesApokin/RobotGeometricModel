eee3383c3fa2a9c78bf2f0b6fca087f2a3d83a89
UlyssesApokin <aoipkn@yandex.ru>
2023-10-05 14:13:43 +0300

    rm clear

--- docs/makepdf.sh
+++ docs/makepdf.sh
@@ -40,13 +40,11 @@ fi
 if [[ "$STATUS" == *"$underlay4pdf"* ]]; then
 	qpdf kurs.pdf --underlay frame-titul-sheet-gost2104-68.pdf --to=1 -- final.pdf
 	qpdf --replace-input --underlay frame-main-sheet-gost2104-68.pdf --to=2 -- final.pdf
-	qpdf --replace-input --underlay frame-сontent-sheet-gost2104-68.pdf --to=3 -- final.pdf
+	qpdf --replace-input --underlay frame-content-sheet-gost2104-68.pdf --to=3 -- final.pdf
 	qpdf --replace-input --underlay frame-main-sheet-gost2104-68.pdf --from= --repeat=1 --to=4-z -- final.pdf
 fi
 
-# Cleaning the screen
 sleep 1
-clear
 
 # Debug output
 if [[ "$STATUS" == *"$dxf2pdf"* ]]; then echo "LibreCAD was launched"; fi
