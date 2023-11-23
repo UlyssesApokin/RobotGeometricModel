9876ff01aa9f1db5805f74a69f64055675b22bb7
Ulysses Apokin <aoipkn@yandex.ru>
2023-11-23 10:50:31 +0300

    renamed func iteration_position_step -> do_iter_step_position

--- source/rgm3_reverse_kinematic.h
+++ source/rgm3_reverse_kinematic.h
@@ -35,7 +35,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define POS 0
 #define NEG 1
 
-double iteration_position_step(double(***f)(double, ...), double final[3][4],
+double do_iter_step_position(double(***f)(double, ...), double final[3][4],
 	int type_q, double delta_q, double q1, ...);
 
 double iteration_orientat_step(double(***f)(double, ...), double final[3][4],
